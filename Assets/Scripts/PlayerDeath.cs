98a9fe231183811d870fb717469550debb082259
Dillon Walls <dillon.walls@slashdotmedia.com>
2020-07-12 13:16:49 -0400

    death and gameover state plus restarting

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public Behaviour[] componentsToDisable;
+
+    public GameObject[] gameObjectsToEnable;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Death")
+        {
+            GameOver();
+            Debug.LogFormat("Dead");
+        }
+    }
+
+    private void GameOver()
+    {
+        this.enabled = false;
+        foreach (var c in componentsToDisable)
+        {
+            c.enabled = false;
+        }
+
+        foreach (var go in gameObjectsToEnable)
+        {
+            go.SetActive(true);
+        }
+    }
+}
